28625e7717f043f0c6eaefa024c02dea0f079414
Zefyro <zefyrogaming@gmail.com>
2023-08-15 16:39:01 +0300

    BogoSort in JavaScript
    An implementation of the BogoSort algorithm in JavaScript

---
+++ javascript/bogosort.js
@@ -0,0 +1,40 @@
+
+BogoSort(GenerateRandomArray(8), 100);
+
+function BogoSort(array, printIterationEvery) {
+    var iteration = 0;
+    PrintIteration(array);
+    while (!IsSorted(array)) {
+        iteration++;
+        array = Shuffle(array);
+        if (iteration % printIterationEvery == 0) { 
+            PrintIteration(array, iteration);
+        }
+    }
+    console.log("Result:");
+    PrintIteration(array, iteration);
+}
+function Shuffle(array) {
+    var c = array.length, i, temp;
+    while (c > 0) {
+        i = Math.floor(Math.random() * c);
+        c--;
+        temp = array[c];
+        array[c] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+function IsSorted(array) {
+    for (i = 1; i < array.length; i++)
+    {
+        if (array[i - 1] > array[i]) { return false; }
+    }
+    return true;
+}
+function GenerateRandomArray(length) {
+    return Array.from({length: length}, () => -100 + Math.floor(Math.random() * 201));
+}
+function PrintIteration(array, iteration = 0) {
+    console.log(`${iteration}, [${array}]`);
+}
