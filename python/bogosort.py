cfc74faa351c9a2fbdbde451ac2659ee3cb32979
Zefyro <zefyrogaming@gmail.com>
2023-08-15 11:44:32 +0300

    BogoSort in Python
    An implementation of the BogoSort algorithm in Python

---
+++ python/bogosort.py
@@ -0,0 +1,37 @@
+import random as rnd
+
+def shuffle(list):
+    i: int = len(list) - 1
+    for num in range(0, i):
+        idx: int = rnd.randint(0, i)
+        list[num], list[idx] = list[idx], list[num]
+    return list
+
+def isSorted(list):
+    i: int = len(list)
+    for num in range(1, i):
+        if list[num - 1] > list[num]:
+            return False
+    return True
+
+def bogosort(list, printIterationEvery: int):
+    iterations: int = 0
+    printIteration(list, iterations)
+    while not isSorted(list):
+        shuffle(list)
+        iterations += 1
+        if iterations % printIterationEvery == 0:
+            printIteration(list, iterations)
+    print("Result:")
+    printIteration(list, iterations)
+
+def printIteration(list, iterations: int):
+    print("{}, {}".format(iterations, list))
+
+def generateRandomList(length: int):
+    list = []
+    for num in range(0, length):
+        list.append(rnd.randint(-100, 100))
+    return list
+
+bogosort(generateRandomList(8), 100)
