b2f72887e1a7e219e1db971afafcad2a05496983
Zefyro <zefyrogaming@gmail.com>
2023-08-20 11:58:06 +0300

    BogoSort in C
    An implementation of the BogoSort algorithm in C

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # BogoSort
 BogoSort is an algorithm used to sort the elements of an array by randomly generating different permutations of an array and then checking whether it is sorted or not. This repository aims to perform this task in all the programming languages that I have learned.
 
-- [x] BogoSort implementation in C#
-- [x] BogoSort implementation in Python
-- [x] BogoSort implementation in Java
-- [x] BogoSort implementation in JavaScript
-- [x] BogoSort implementation in Dart
+- [x] BogoSort implementation in C
 - [x] BogoSort implementation in C++
+- [x] BogoSort implementation in C#
+- [x] BogoSort implementation in Rust
+- [x] BogoSort implementation in Java
+- [x] BogoSort implementation in Dart
+- [x] BogoSort implementation in Python
+- [x] BogoSort implementation in JavaScript
 - [x] BogoSort implementation in [JMC](https://github.com/WingedSeal/jmc) [^1]
 - [x] BogoSort implementation in SQF
-- [x] BogoSort implementation in Rust
 
 All versions of the BogoSort algorithm will be done following the pseudocode:
 
---
+++ c/bogosort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printIteration(int* array, int* length, int* iteration) {
+    printf("%d, [", *iteration);
+    for (int i = 0; i < *length; i++) {
+        printf("%d", array[i]);
+        if (i + 1 < *length) printf(", ");
+    }
+    printf("]\n");
+}
+
+int* generateRandomArray(int length) {
+    srand (time(NULL));
+    int* array = malloc(length * sizeof(int));
+    for (int i = 0; i < length; i++) {
+        array[i] = rand() % 200 - 100;
+    }
+    return array;
+}
+
+int isSorted(int* array, int* length) {
+    for (int i = 1; i < *length; i++) {
+        if (array[i - 1] > array[i]) 
+            return 0;
+    }
+    return 1;
+}
+
+int* shuffle(int* array, int* length) {
+    for (int i = 0; i < *length; i++) {
+        int j = rand() % *length;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+void bogosort(int length, int printIterationsEvery) {
+    int* array = generateRandomArray(length);
+    int iteration = 0;
+    printIteration(array, &length, &iteration);
+    while (!isSorted(array, &length)) {
+        iteration++;
+        array = shuffle(array, &length);
+        if (iteration % printIterationsEvery == 0)
+            printIteration(array, &length, &iteration);
+    }
+    printf("Result:\n");
+    printIteration(array, &length, &iteration);
+}
+
+int main()
+{
+    bogosort(8, 100);
+    return 0;
+}
